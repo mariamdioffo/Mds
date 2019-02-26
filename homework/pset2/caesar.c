f041a62c32c7145df03752347fb1218a1d669e4a
mariamdioffo <48026921+mariamdioffo@users.noreply.github.com>
2019-02-26 18:16:58 +0100

    Add files via upload

---
+++ homework/pset2/caesar.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+    int main(int argc, string argv[])
+{
+if(argc==2)
+{
+    int k=atoi(argv[1]);
+    string plaintext=get_string("plaintext: ");
+    printf("cyphertext: ");
+    for(int i=0; i<(strlen(plaintext)); i++)
+
+        if(isalpha(plaintext[i]))
+            {
+                if(isupper(plaintext[i]))
+                {
+                    printf("%c",(((plaintext[i]- 65)+ k)%26)+ 65);
+                }
+                if(islower(plaintext[i]))
+                {
+                    printf("%c",(((plaintext[i]- 97)+ k)%26)+ 97);
+                }
+            }
+        else
+        {
+            printf("%c",plaintext[i]);
+        }
+}
+else
+{
+    printf("Invalid");
+    return 0;
+}
+}
